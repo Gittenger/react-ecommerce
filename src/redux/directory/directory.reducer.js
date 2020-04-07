de6faff8baea7236876d2bcf92ca4074344c6d81
John Pittenger <john.pittenger86@gmail.com>
2020-04-07 19:15:06 -0400

    refactored directory/shop to use redux, redux persist for local storage

---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,12 @@
+import data from "./directory.data";
+
+const INITIAL_STATE = data;
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
