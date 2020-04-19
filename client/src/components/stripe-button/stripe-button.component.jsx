d2ac3667c186ac54a007e6a1a7b7518077e66d81
John Pittenger <john.pittenger86@gmail.com>
2020-04-19 00:34:25 -0400

    linked stripe to server and cart clear on checkout

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { checkoutSuccess } from "../../redux/cart/cart.actions";
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, checkoutSuccess }) => {
   const priceInCents = price * 100;
   const publicKey = "pk_test_pulLUU0iOLGSheoPetikthTC00JqLYCAzw";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceInCents,
+        token,
+      },
+    })
+      .then((res) => {
+        alert("Payment successful");
+        checkoutSuccess();
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you used the provided test credit card."
+        );
+      });
   };
 
   return (
@@ -26,4 +45,8 @@ const StripeCheckoutButton = ({ price }) => {
   );
 };
 
-export default StripeCheckoutButton;
+const mapDispatchToProps = (dispatch) => ({
+  checkoutSuccess: () => dispatch(checkoutSuccess()),
+});
+
+export default connect(null, mapDispatchToProps)(StripeCheckoutButton);
--- client/src/redux/cart/cart.sagas.js
+++ client/src/redux/cart/cart.sagas.js
@@ -2,6 +2,7 @@ import { takeLatest, put, all, call } from "redux-saga/effects";
 
 import { clearCart } from "./cart.actions";
 import UserActionTypes from "../user/user.types";
+import CartActionTypes from "./cart.types";
 // import CartActionTypes from "./cart.types";
 
 export function* clearCartItems() {
@@ -12,6 +13,10 @@ export function* onSignOutSuccess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartItems);
 }
 
+export function* onCheckoutSuccess() {
+  yield takeLatest(CartActionTypes.CHECKOUT_SUCCESS, clearCartItems);
+}
+
 export function* cartSagas() {
-  yield all([call(onSignOutSuccess)]);
+  yield all([call(onSignOutSuccess), call(onCheckoutSuccess)]);
 }
